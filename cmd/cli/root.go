ded336782fded9c0fac47503bad426d805c71d2f
Cristian Giraldo Zúñiga <c.giraldoz@uniandes.edu.co>
2024-11-01 21:46:06 -0500

    Changed cobra commands initialization

--- cmd/cli/root.go
+++ cmd/cli/root.go
@@ -6,29 +6,26 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var (
-	redisCache cache.Cache
-	rootCmd    = &cobra.Command{
-		Use:     "gip [ip]",
-		Short:   "A CLI for querying IP address geolocation data.",
-		Long:    "Geo IP Info is a CLI for querying IP address geolocation data.",
-		Example: "gip 0.0.0.0\ngip stats",
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) == 1 {
-				fmt.Printf("Consulting geolocation for IP: %s\n", args[0])
-			} else {
-				fmt.Println("Please provide a valid IP address or command.")
-			}
-		},
-	}
-)
+var rootCmd = &cobra.Command{
+	Use:     "gip [ip]",
+	Short:   "A CLI for querying IP address geolocation data.",
+	Long:    "Geo IP Info is a CLI for querying IP address geolocation data.",
+	Example: "gip 0.0.0.0\ngip stats",
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) == 1 {
+			fmt.Printf("Consulting geolocation for IP: %s\n", args[0])
+		} else {
+			fmt.Println("Please provide a valid IP address or command.")
+		}
+	},
+}
 
-func init() {
+func InitializeCommands(redisCache cache.Cache) {
 	rootCmd.AddCommand(NewStatsCmd(redisCache))
 	rootCmd.AddCommand(NewApiCmd(redisCache))
 }
 
-func Execute(rc cache.Cache) error {
-	redisCache = rc
+func Execute(redisCache cache.Cache) error {
+	InitializeCommands(redisCache)
 	return rootCmd.Execute()
 }
